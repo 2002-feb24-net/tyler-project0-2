1bda8422cd6d04cb423637d7bfd75fb71f37632b
shawnhalcomb <shawnhalcomb@gmail.com>
2020-04-01 10:36:24 -0500

    seperated login/register system

--- Project0.Data/CustomerClassHelper.cs
+++ Project0.Data/CustomerClassHelper.cs
@@ -38,38 +38,6 @@ namespace Project0.App
         }
 
 
-        public Customer AddCustomer()
-        {
-            var cust1 = new Customer();
-            try
-            {
-                Console.Write("What is your first name: ");
-                cust1.FirstName = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write("What is your last name: ");
-                cust1.LastName = Console.ReadLine();
-                Console.WriteLine();
-                UserNamePass();
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("You didn't the connect type");
-                AddCustomer();
-            }
-
-            ctx.Add(cust1);
-            try
-            {
-                ctx.SaveChanges();
-                Console.WriteLine("Added to Database.");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Save didn't work.");
-            }
-
-            return cust1;
-        }
 
 
         public void SearchCustoemrOrderInStore()
@@ -96,32 +64,7 @@ namespace Project0.App
             }
         }
 
-        public void UserNamePass()
-        {
-            var cust1 = new Customer();
-            string password2 = "";
 
-
-            try
-            {
-                Console.Write("Enter a username: ");
-                cust1.UserName = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write("Enter a password: ");
-                cust1.Password = Console.ReadLine();
-                Console.WriteLine();
-                Console.Write("Verify password: ");
-                password2 = Console.ReadLine();
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("You enter a wrong character. Try again");
-                UserNamePass();
-            }
-
-            if (cust1.Password != password2)
-                UserNamePass();
-        }
 
         public void SearchForCustomerOrder()
         {
@@ -148,38 +91,6 @@ namespace Project0.App
             }
         }
 
-        public Customer OldCustomer()
-        {
-            using (var ct = new Project0Context())
-            {
-                try
-                {
-                    Console.Write("What is your username: ");
-                    string text = Console.ReadLine();
-                    Console.WriteLine();
-                    Console.Write("What is your password: ");
-                    string text2 = Console.ReadLine();
 
-                    var customer = ct.Customer.FirstOrDefault(a => a.UserName == text);
-                    if (customer.Password != text2)
-                    {
-                        Console.WriteLine("Sorry Username and Password didn't match. Try again");
-                        return OldCustomer();
-
-                    }
-                    return customer;
-                }
-                catch (NullReferenceException)
-                {
-                    string text3 = "";
-
-                    Console.WriteLine("Couldn't find that user:");
-                    //Again(text3, c1);
-                    return new Customer(); // all null values
-
-                }
-
-            }
-        }
     }
 }
---
+++ Project0.Data/UserSystem.cs
@@ -0,0 +1,109 @@
+﻿using Project0.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project0.Data
+{
+    public class UserSystem
+    {
+        private static readonly Project0Context ctx = new Project0Context();
+
+        public Customer OldCustomer()
+        {
+            using (var ct = new Project0Context())
+            {
+                try
+                {
+                    Console.Write("What is your username: ");
+                    string text = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.Write("What is your password: ");
+                    string text2 = Console.ReadLine();
+
+                    var customer = ct.Customer.FirstOrDefault(a => a.UserName == text);
+                    if (customer.Password != text2)
+                    {
+                        Console.WriteLine("Sorry Username and Password didn't match. Try again");
+                        return OldCustomer();
+
+                    }
+                    return customer;
+                }
+                catch (NullReferenceException)
+                {
+                    string text3 = "";
+
+                    Console.WriteLine("Couldn't find that user:");
+                    //Again(text3, c1);
+                    return new Customer(); // all null values
+
+                }
+
+            }
+        }
+
+
+        public Customer AddCustomer()
+        {
+            var cust1 = new Customer();
+            try
+            {
+                Console.Write("What is your first name: ");
+                cust1.FirstName = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("What is your last name: ");
+                cust1.LastName = Console.ReadLine();
+                Console.WriteLine();
+                UserNamePass();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You didn't the connect type");
+                AddCustomer();
+            }
+
+            ctx.Add(cust1);
+            try
+            {
+                ctx.SaveChanges();
+                Console.WriteLine("Added to Database.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Save didn't work.");
+            }
+
+            return cust1;
+        }
+
+        public void UserNamePass()
+        {
+            var cust1 = new Customer();
+            string password2 = "";
+
+
+            try
+            {
+                Console.Write("Enter a username: ");
+                cust1.UserName = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter a password: ");
+                cust1.Password = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Verify password: ");
+                password2 = Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You enter a wrong character. Try again");
+                UserNamePass();
+            }
+
+            if (cust1.Password != password2)
+                UserNamePass();
+        }
+
+    }
+}
